b93f4b8bb884c97bf6ad348410dbef57846d1c2a
AD-amd <andrei.dubovoi@amdaris.com>
2022-11-26 12:05:50 +0200

    working 1 case Manager

--- Echo.Bot/Bots/EchoBot.cs
+++ Echo.Bot/Bots/EchoBot.cs
@@ -1,5 +1,6 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder;
@@ -13,7 +14,19 @@ public class EchoBot : ActivityHandler
 		ITurnContext<IMessageActivity> turnContext,
 		CancellationToken cancellationToken)
 	{
-		var replyText = $"Response: {turnContext.Activity.Text}";
+		var response_message = "";
+		var patern_for_manager = @"(?#\s*\W*\s*\w*\s*)(?:[Manager]{7})|(?:[manager]{7})(?#\s*\W*\s*\w*\s*)";
+		switch (Regex.Match(turnContext.Activity.Text, patern_for_manager, RegexOptions.Compiled).ToString())
+		{
+			case "manager":
+				response_message = "Your Line Manager is " + "https://projectplan.amdaris.com/profile";
+				break;
+			case "Manager":
+				response_message = "Your Line Manager is " + "https://projectplan.amdaris.com/profile";
+				break;
+
+				}
+		string replyText = $"Response: {response_message}";
 		Debug.WriteLine(replyText);
 		var response = await turnContext.SendActivityAsync(
 			MessageFactory.Text(replyText, replyText),
